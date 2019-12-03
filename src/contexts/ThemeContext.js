1c42745b4e1a37a081222d7d639026b462ec3d47
Nicolas Le Pasteur <nicolas.lepasteur@gmail.com>
2019-12-03 09:38:31 +0100

    context type + consumer

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -10,10 +10,18 @@ class ThemeContextProvider extends Component {
     dark: { syntax: '#ddd', ui: '#333', bg: '#555' }
   }
   render() {
-    console.log(MyContext)
+    console.log('I am the context : ', MyContext)
     return (
       <MyContext.Provider value={{ ...this.state }}>
         {this.props.children}
+        {/* childrens définis dans app correspondent en fait à
+          <ThemeContextProvider >
+             <MyContext.Provider>
+             <Les components héritant du state soit static contextType ou MyContext.Consumer />
+          </MyContext.Provider>
+        </ThemeContextProvider>
+        */}
+        {/* Je pense qu'ici props correspondent à components passés entre tags de ThemeContextProvider, apprès tout, props souvent passées comme dans composent dans balise ouvrante et fermante */}
       </MyContext.Provider>
     )
   }
